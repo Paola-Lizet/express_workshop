abe2605af6b2b278ea640c1e01f11ef8b598cd6e
Paola Lizet <pjimenez05@alumnos.uaq.mx>
2026-04-25 22:00:54 -0600

    Actualización

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 //Es secuencial
+//Dependencies 
 const morgan = require('morgan');
 const express = require("express");
 const app = express();
+//Routers
 const pokemon = require('./routes/pokemon');
 const user = require('./routes/user');
+//Middleware
+const auth = require('./middleware/auth');
+const notFound = require('./middleware/notFound');
+const indexDos = require('./middleware/indexDos');
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-
-app.get("/", (req, res, next) => {
-    return res.status(200).json({code: 1, message: "Bienvenido al pokedex"});
-});
-
-app.use("/pokemon", pokemon);
+app.get("/", indexDos);
 app.use("/user", user);
-
-app.use((req, res, next) => {
-    return res.status(404).json({code: 404, message: "URL no encontrada"});
-})
+app.use(auth);
+app.use("/pokemon", pokemon);
+app.use(notFound);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running...")
